cbbb56c14b2bb3193c39f52a8fd096798cfd95e0
nabillahumi <nabillahumi003@gmail.com>
2024-09-30 21:19:21 +0700

    jobsheet 5 pemilihan 1

--- PemilihanHariDenganIf18.java
+++ PemilihanHariDenganIf18.java
@@ -1,22 +1,22 @@
-import java.util.Scanner;
+    import java.util.Scanner;
 
-public class PemilihanHariDenganIf18 {
-    
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    public class PemilihanHariDenganIf18 {
+        
+        public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
 
-        int angka;
+            int angka;
 
-        System.out.print("Masukkan angka (1-7): ");
-        angka = sc.nextInt();
+            System.out.print("Masukkan angka (1-7): ");
+            angka = sc.nextInt();
 
-        if (angka >= 1 && angka <= 5) {
-            System.out.println("Weekday");
-        } else if (angka == 6 || angka == 7) {
-            System.out.println("Weekend");
-        } else {
-            System.out.println("Invalid Number");
+            if (angka >= 1 && angka <= 5) {
+                System.out.println("Weekday");
+            } else if (angka == 6 || angka == 7) {
+                System.out.println("Weekend");
+            } else {
+                System.out.println("Invalid Number");
+            }
+
         }
-
     }
-}
